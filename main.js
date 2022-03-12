0b52cc8b8e227f810f33f47d4371b32db617e5a8
Sionna Poleszuk <sionna.pol@gmail.com>
2022-03-12 21:20:48 +0100

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 'use strict';
+
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
+
 function main() {
+
   // create new window
   let mainWindow = new BrowserWindow({
     webPreferences: {
@@ -9,13 +12,17 @@ function main() {
     },
     width: 520,
     height: 650,
-    frame: false
+    frame:false,
   })
+
   // load app/index.html as the window content
   mainWindow.loadFile(path.join('app', 'index.html'));
+  mainWindow.webContents.openDevTools();
+
 }
 
 app.on('ready', main);
+
 app.on('window-all-closed', function () {
   app.quit();
 });
